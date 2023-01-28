eb2acc9f82bf257fe3f061c18fb64838b7121f05
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2023-01-28 13:27:50 +0330

    add board and colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import './index.css'
+const width = 8;
+const candyColors = ["blue", "green", "orange", "red", "purple", "yellow"];
 
-function App() {
+
+
+const App = () => {
+
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+        randomColorArrangement.push(randomColor)
+    }
+    setCurrentColorArrangement(randomColorArrangement)
+  };
+  
+  useEffect(() => {
+    createBoard()
+  }, [])
+
+console.log(currentColorArrangement)
+
   return (
-    <div className="App">
-      
-      hi
+  <div className="app">
+    <div className="game">
+      {currentColorArrangement.map((candyColors , index) => (
+        <img
+        key={index}
+        style={{backgroundColor:candyColors}}
+        />
+      ))}
     </div>
-  );
-}
 
+  </div>
+
+)
+      }
 export default App;
